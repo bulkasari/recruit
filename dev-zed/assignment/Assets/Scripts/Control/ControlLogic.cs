017d258f1b548e29277679b4d40736754cdaf484
DESKTOP-SDIV8MB\user <bulkasari@gmail.com>
2021-03-18 22:03:28 +0900

    add data parsing

--- dev-zed/assignment/Assets/Scripts/Control/ControlLogic.cs
+++ dev-zed/assignment/Assets/Scripts/Control/ControlLogic.cs
@@ -1,14 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlLogic : MonoBehaviour
 {
+    ModelApi mModelDong;
     public void OnAwake()
     {
-        Debug.Log("asdfafsd");
-        string sContent = Resources.Load("Samples/json/dong").ToString();
-        Debug.Log(sContent);
+        string strContent = Resources.Load("Samples/json/dong").ToString();
+        strContent = strContent.Replace("동", "bd_num");
+        strContent = strContent.Replace("지면높이", "height");
+        strContent = strContent.Replace("룸타입id", "roomtype_id");
+
+        mModelDong = JsonUtility.FromJson<ModelApi>(strContent);
+        List<ModelDong> listData = mModelDong.data;
+        for (int i = 0; i < listData.Count; ++i)
+        {
+            ModelDong dataDong = listData[i];
+            for (int j = 0; j < dataDong.roomtypes.Count; ++j)
+            {
+                byte[] buffer = Convert.FromBase64String(dataDong.roomtypes[j].coordinatesBase64s[0]);
+                float[] samples = new float[buffer.Length];
+                Buffer.BlockCopy(buffer, 0, samples, 0, buffer.Length);
+
+                for (int num = 0; num < samples.Length; ++num)
+                {
+                    Debug.Log(samples[num]);
+                }
+            }
+            //Debug.Log(listData[i].roomtypes.coordinatesBase64s);
+            //Debug.Log(listData[i].roomtypes.meta.roomtype_id);
+            //Debug.Log(listData[i].meta.bd_num);
+            //Debug.Log(listData[i].meta.height);
+        }
     }
     public void OnUpdate()
     {
---
+++ dev-zed/assignment/Assets/Scripts/Models/ModelDong.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ModelApi
+{
+    public bool success;
+    public int code;
+    public List<ModelDong> data;
+}
+
+[Serializable]
+public class ModelDong
+{
+    public List<ModelRoomTypes> roomtypes;
+    public ModelDongMeta meta;
+}
+
+[Serializable]
+public class ModelRoomTypes
+{
+    public List<string> coordinatesBase64s;
+    public ModelRoomTypesMeta meta;
+}
+
+[Serializable]
+public class ModelRoomTypesMeta
+{
+    public int roomtype_id;
+}
+
+[Serializable]
+public class ModelDongMeta
+{
+    public int bd_id;
+    public int bd_num;
+    public int height;
+}
