956f62f560034866c4710e0648ef47914849d398
DESKTOP-SDIV8MB\user <bulkasari@gmail.com>
2021-03-18 20:50:24 +0900

    add base framework

---
+++ dev-zed/assignment/Assets/Scripts/Control/ControlLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLogic : MonoBehaviour
+{
+    public void OnAwake()
+    {
+        Debug.Log("asdfafsd");
+        string sContent = Resources.Load("Samples/json/dong").ToString();
+        Debug.Log(sContent);
+    }
+    public void OnUpdate()
+    {
+
+    }
+}
---
+++ dev-zed/assignment/Assets/Scripts/MyFramework.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFramework : MonoBehaviour
+{
+    ControlLogic mControLogic;
+    void Awake()
+    {
+        mControLogic = gameObject.AddComponent<ControlLogic>();
+        mControLogic.OnAwake();
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        mControLogic.OnUpdate();
+    }
+}
